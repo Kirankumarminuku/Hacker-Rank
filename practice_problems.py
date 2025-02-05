8bed2146216d012d86c6688588966e5440158495
Kiran Kumar Minuku <84305484+Kirankumarminuku@users.noreply.github.com>
2025-02-05 16:49:36 +0530

    Update practice_problems.py

--- practice_problems.py
+++ practice_problems.py
@@ -75,3 +75,73 @@ Print n lines, one corresponding to each i.
 n = int(input())
 for i in range(0, n):
     print(i*i)
+
+"""
+6.Task : Write a Function
+An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+In the Gregorian calendar, three conditions are used to identify leap years:
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. 
+
+Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+
+Input Format
+Read year, the year to test.
+
+Output Format
+The function must return a Boolean value (True/False). Output is handled by the provided code stub.
+"""
+#Solution
+def is_leap(year):
+    leap = False
+    
+    if (year % 400 == 0) and (year % 100 == 0):
+        leap = True
+    elif (year % 4 ==0) and (year % 100 != 0):
+        leap = True
+    else:
+        leap = False
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+"""
+7. Task : Prit Function
+Without using any string methods, try to print the following:
+123....n
+Note that "....." represents the consecutive values in between.
+
+Example
+n = 5
+Print the string 12345.
+"""
+#Solution
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
+"""
+8.Task : Find the Runner-Up Score
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+
+Input Format
+The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+Output Format
+Print the runner-up score.
+"""
+#Solution
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+arr1 = list(set(arr))
+arr1.sort()
+
+print(arr1[-2])
